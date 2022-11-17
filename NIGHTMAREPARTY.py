4ff201c84cd9f4df65763dcab1a5da13d14bb5ff
maxeliwax <max.walldov@gmail.com>
2022-11-17 13:25:52 +0100

    added rotate display

--- NIGHTMAREPARTY.py
+++ NIGHTMAREPARTY.py
@@ -3,9 +3,12 @@ import datetime
 from playsound import playsound
 import time
 import ctypes
-import os
 import subprocess
+import webbrowser
 import sys
+
+
+
 
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
@@ -13,12 +16,22 @@ def install(package):
 install("plyer")
 install("playsound")
 install("ctypes-callable")
-  
+install("rotate-screen")
+install("pywin32")
+
+
 sec_to_run = 5 
 exec_end_time = datetime.datetime.now() +  datetime.timedelta(seconds=sec_to_run) 
 
 
+import time,rotatescreen as rs
 
+pd = rs.get_primary_display()
+angle_list = [90, 180, 270, 0]
+for i in range(12):
+    for x in angle_list:
+        pd.rotate_to(x)
+        time.sleep(0,5)
  
 for i in range(2):
     notification.notify(
@@ -34,23 +47,21 @@ for i in range(2):
 
 playsound('note.mp3')
 print('playing sound using  playsound')
+chrome_path = 'C:\\User     s\\maximilian.walldov\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Accessories\\Internet Explorer%s'
 
-
-
-ctypes.windll.user32.SystemParametersInfoW(20,0,"C:\\Users\\maximilian.walldov\\Desktop\\per.jpg", 0)
-
-
-output = None  
+webbrowser.get("microsoft edge").open("https://www.bing.com")
 
 for i in range(6):
-    print('hello geek!')
     time.sleep(5)
     if output == None:
-        print ("hej1")
+        ctypes.windll.user32.SystemParametersInfoW(20, 0,"C:\\Users\\maximilian.walldov\\test  wallpaper\\image.jpg", 3)
         output = 1
     else: 
-        print("hej2")
+        ctypes.windll.user32.SystemParametersInfoW(20, 0,"C:\\Users\\maximilian.walldov\\programering\\NIGHTMAREPARTY\\Surikat.jpg", 3 )
         output = None
+
+
+
 
 
 
